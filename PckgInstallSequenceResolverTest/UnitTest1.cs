68af57120c9e282b9c3c3e6b937a22878820191a
Yogendra-khopkar <Yogendra.khopkar@gmail.com>
2018-09-24 11:01:08 -0600

    Adding the test project

---
+++ PckgInstallSequenceResolverTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PckgInstallSequenceResolverTest
+{
+	[TestClass]
+	public class UnitTest1
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+		}
+	}
+}
