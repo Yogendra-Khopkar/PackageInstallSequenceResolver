4c2b5d3c619e7c401a7be7085ed446591d02d355
Yogendra-khopkar <Yogendra.khopkar@gmail.com>
2018-09-24 11:29:57 -0600

    Adding parser to parse the input

---
+++ PckgInstallSequenceResolver/ArrayInputParser.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace PckgInstallSequenceResolver
+{
+	public class ArrayInputParser : IInputParser
+	{
+		public IEnumerable<string> ParseInput(string[] input)
+		{
+			throw new System.NotImplementedException();
+		}
+	}
+}
---
+++ PckgInstallSequenceResolver/IInputParser.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace PckgInstallSequenceResolver
+{
+	public interface IInputParser
+	{
+		IEnumerable<string> ParseInput(string[] input);
+	}
+}
